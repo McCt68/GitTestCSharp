de92df0814659282dedb6b3b8a8c9d05050c116a
mcct <mcct@privat.dk>
2023-01-30 20:32:09 +0100

    Second Branch

--- GitTestCSharp/Program.cs
+++ GitTestCSharp/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // SECOND BRANCH
+            Console.WriteLine("Second local branch");
         }
     }
 }
